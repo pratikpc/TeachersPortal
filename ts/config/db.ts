625b7735734dee2c9fc51b2d3ec4f34003a097db
Pratik Chowdhury <pratikc@live.co.uk>
2020-04-20 16:32:33 +0530

    Now also supports .env

--- ts/config/db.ts
+++ ts/config/db.ts
@@ -1,9 +1,9 @@
 export const DB = {
-  Dialect: "postgres",
-  ProjectName: "teacherportal",
-  DatabaseName: "postgres",
-  Host: "localhost",
-  UserName: "postgres",
-  Port: 5432,
-  Password: "postgres",
+  Host: String(process.env.DATABASE_HOST || "localhost"),
+  Port: Number(process.env.DATABSE_PORT || 542),
+  UserName: String(process.env.DATABASE_USER_NAME || "postgres"),
+  Password: String(process.env.DATABASE_PASSWORD || "postgres"),
+  DatabaseName: String(process.env.DATABASE_NAME || "teacherportal"),
+  ssl: Boolean(String(process.env.DATABSE_SSL) === "true" || false)
 };
+console.log(DB);
