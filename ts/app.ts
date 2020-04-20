d69aa53474083a9b2950a46116f04030653fe977
Pratik Chowdhury <pratikc@live.co.uk>
2020-04-20 16:34:32 +0530

    Server is now async

--- ts/app.ts
+++ ts/app.ts
@@ -5,42 +5,45 @@ import * as Routes from "./routes/Routes";
 import * as cors from "cors";
 import { PassportModelsGenerate } from "./Models/Passport.Models";
 
-export const app = express();
-// app.use(bodyParser.json());
-// middleware for parsing application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+export async function App() {
+    const app = express();
+    // app.use(bodyParser.json());
+    // middleware for parsing application/x-www-form-urlencoded
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-// Use this to Render HTML
+    // Use this to Render HTML
 
-// Specify Path of Website Static Contents
-app.engine('.html', require('ejs').renderFile);
-app.set("views", "./Views");
-app.set('view engine', 'ejs');
-app.use("/", express.static("./Website"));
+    // Specify Path of Website Static Contents
+    app.engine('.html', require('ejs').renderFile);
+    app.set("views", "./Views");
+    app.set('view engine', 'ejs');
+    app.use("/", express.static("./Website"));
 
-Models.RunSynchronisation().then(() => { });
-PassportModelsGenerate(app);
+    await Models.RunSynchronisation();
+    await PassportModelsGenerate(app);
+    
+    app.use(cors.default());
+    // middleware for json body parsing
+    app.use(bodyParser.json());
+    // middleware for parsing application/x-www-form-urlencoded
+    app.use(bodyParser.urlencoded({ extended: true }));
+    // middleware for json body parsing
+    app.use(bodyParser.json({ limit: "20mb" }));
 
-app.use(cors.default());
-// middleware for json body parsing
-app.use(bodyParser.json());
-// middleware for parsing application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-// middleware for json body parsing
-app.use(bodyParser.json({ limit: "20mb" }));
+    // Route via this as Path to Users
+    app.use("/user", Routes.Users);
+    // Route via this as Path to Admin
+    app.use("/admin", Routes.Admin);
 
-// Route via this as Path to Users
-app.use("/user", Routes.Users);
-// Route via this as Path to Admin
-app.use("/admin", Routes.Admin);
-
-// Route via this as Path to Updation
-app.use("/", Routes.Updation);
-// Route via this as Path to Conference
-app.use("/", Routes.Conference);
-app.use("/", Routes.Fdp);
-app.use("/", Routes.Sttp);
-app.use("/", Routes.Journal);
-app.use("/", Routes.Progatt);
-app.use("/", Routes.Semwork);
-app.use("/", Routes.Mrg);
+    // Route via this as Path to Updation
+    app.use("/", Routes.Updation);
+    // Route via this as Path to Conference
+    app.use("/", Routes.Conference);
+    app.use("/", Routes.Fdp);
+    app.use("/", Routes.Sttp);
+    app.use("/", Routes.Journal);
+    app.use("/", Routes.Progatt);
+    app.use("/", Routes.Semwork);
+    app.use("/", Routes.Mrg);
+    return app;
+}
--- ts/server.ts
+++ ts/server.ts
@@ -1,11 +1,19 @@
+import * as dotenv from "dotenv";
+import { existsSync } from "fs";
+
+if (existsSync(".env")) {
+  console.log("Using .env file to supply config environment variables");
+  dotenv.config({ path: ".env" });
+}
 import * as config from './config/server'
-import {app} from "./app";
+import { App } from "./app";
 
-app.listen(config.Server.Port, config.Server.Name, () => {
+App().then(app => app.listen(config.Server.Port, config.Server.Name, () => {
   console.log("Default Login Screen", config.Server.Name + ':' + config.Server.Port);
-});
+})
+);
 
-process.on("SIGINT", function() {
+process.on("SIGINT", function () {
   console.log("App Shutting Down");
 });
 
